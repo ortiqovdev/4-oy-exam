1185d78308f56a31243f2b39a2fed31944e9152d
Akmal Ortiqov <116548309+ortiqovdev@users.noreply.github.com>
2023-03-07 23:19:13 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
-import express, { Express, request, response } from 'express';
-import { Data, malumotlar } from './data';
+import express, { Express, response } from "express";
+import { Data, users } from "./data";
 import { v4 } from 'uuid'
+import { request } from "http";
+import { send } from "process";
 
-const server: Express = express();
+const server: Express = express()
 server.use(express.json())
 server.use(express.urlencoded())
 
 server.post('/register', (request, response) => {
-
     let register: Data = {
         id: v4(),
         name: request.body.name,
@@ -15,53 +16,53 @@ server.post('/register', (request, response) => {
         email: request.body.email,
         age: request.body.age,
         password: request.body.password
+
     }
-    const pochta = malumotlar.find(pochta => pochta.email === request.body.email)
+    const emailCheck = users.find(emailCheck => emailCheck.email === request.body.email)
+    users.push(register)
 
-    if (pochta == undefined) {
-        malumotlar.push(register)
-
+    if (emailCheck == undefined) {
         response
             .status(200)
-            .send("User added")
-    }else{
-        response
-        .status(404)
-        .send('xato')
+            .send(`${request.body.email} added`)
     }
-})
-
-server.post('/login', (request, response) => {
-    const pochta = malumotlar.find(pochta => pochta.email === request.body.email)
-    const parol = malumotlar.find(parol => parol.password === request.body.password)
-    if (pochta == undefined) {
-        response
-            .status(404)
-            .send("User not found")
-    } 
-    // else if (parol?.password != undefined) {
-    //     response
-    //         .status(404)
-    //         .send("password is wrong")
-    // }
     else {
         response
-            .status(200)
-            .send(pochta)
+            .status(404)
+            .send(`${request.body.email} bu email mavjud.`)
     }
 })
-
-
+server.post('/login', (request, response) => {
+    const email1 = users.find(email1 => email1.email === request.body.email)
+    const parol = users.find(parol => parol.password === request.body.password)
+    if (email1 == undefined) {
+        response
+            .status(404)
+            .send("user not found")
+    } else if (parol == undefined) {
+        response
+            .status(404)
+            .send("passwor wrong")
+    } else {
+        response
+            .status(200)
+            .send(email1)
+    }
+})
 
 
 server.get('/users', (request, response) => {
+
     response
         .status(200)
-        .send(malumotlar)
+        .send({
+            massage: "All users:",
+            users
+        })
 })
 
 
-server.listen(2111, () => {
-    console.log("Server working... (Port:2111)");
+server.listen(1111, () => {
+    console.log("Server worikng... (Port:1111)");
 
 })
